941c4fa23e41f949988c860a017d71916545ce63
Vikum Viraj <99325434+IT21161810@users.noreply.github.com>
2022-03-21 15:37:10 +0000

    what did you change

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,40 @@
 #include "Box.h"
+#include <iostream>
+
 
 // Implement setters and getters
 
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+      return length * width * height;
+}
+
+void Box::setLenght(double l)
+{
+   Length = l;
+}
+
+void Box::setWidth(double w)
+{
+   Width = w;
+}
+
+void Box::setHeight(double h)
+{
+   Height = h;
+}
+
+int Box::getLength()
+{
+  return length;
+}
+
+int Box::getWidth()
+{
+  return width;
+}
+int Box::getHeight()
+{
+  return height;
+  
 }
--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,24 @@
 #include "Student.h"
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int pstudentId , const char pname[]) 
+{
+    pstudentId = studentId;
+    strcpy(name,pname);
   
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display()
+{
+
+
+  cout << "Enter student id : " << studentId << endl;
+  cout << "Enter student : " << name << endl;
+  
   
 }
